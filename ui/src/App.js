6c7b8483b8ad628fa3a25513ac41901eb5f941cb
Alberto Chang <me@changalberto.com>
2019-04-23 00:52:54 -0400

    Theme MUI

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,11 +1,29 @@
 import React, { Component } from 'react';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import blue from '@material-ui/core/colors/blue';
+import { pink } from '@material-ui/core/colors';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import './App.scss';
 import {Dashboard} from './screens';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    secondary: pink
+  },
+  typography: {
+    useNextVariants: true,
+  },
+});
 
 class App extends Component {
   render() {
     return (
-      <Dashboard />
+      <MuiThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Dashboard />
+      </MuiThemeProvider>
     );
   }
 }
